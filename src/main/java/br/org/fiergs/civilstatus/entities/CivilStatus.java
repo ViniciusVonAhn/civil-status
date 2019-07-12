26ba531de977b1b6e9e283a86b76b164023d4b5b
Eduardo Hernandorena <eduuardobh@gmail.com>
2019-07-12 14:05:07 -0300

    inserção da chave

--- src/main/java/br/org/fiergs/civilstatus/entities/CivilStatus.java
+++ src/main/java/br/org/fiergs/civilstatus/entities/CivilStatus.java
@@ -6,27 +6,28 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
-@Table(name = "CAD_CIVILSTATUS")
-@SequenceGenerator(name = "seqCad_civilstatus", sequenceName = "SEQCAD_CIVILSTATUS", allocationSize = 1)
+@Table(name = "CCOR_CIVILSTATUS")
+@SequenceGenerator(name = "seqCCor_civilstatus", sequenceName = "SEQCCOR_CIVILSTATUS", allocationSize = 1)
 public class CivilStatus {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seqCad_civilstatus")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "seqCCor_civilstatus")
     private Long id;
 
     @NotEmpty(message = "Nome é obrigatório")
+    @Max(value = 30, message = "O nome deve conter no máximo 30 caracteres!")
     private String name;
 
-    @NotNull(message = "Identificador é obrigatório")
-    private Long identifier;
+    @Max(value = 30, message = "A chave deve conter no máximo 30 caracteres!")
+    private String key;
 
     private boolean status;
 }
